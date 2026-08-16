7aaa8b50d5bc4f6d2c84a5bc3ea01b7a6675c698
vanvan <979167946@qq.com>
2026-08-16 23:34:58 +0800

    the first commit

---
+++ command.md
@@ -0,0 +1,3 @@
+git init   初始化git仓库
+git status  查看管理
+git add  添加管理
---
+++ script.js
@@ -0,0 +1,4 @@
+function changeText() {
+        const msg = document.getElementById('msg');
+        msg.textContent = '你点击了按钮，网页内容已更新！';
+      }
